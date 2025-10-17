9ccc771abd41a71743231e2881db3d273db3e470
Nehuuln <maximeye1234@gmail.com>
2025-10-17 14:47:04 +0200

    fix entity livre

--- src/Entity/Emprunt.php
+++ src/Entity/Emprunt.php
@@ -20,6 +20,10 @@ class Emprunt
     #[ORM\JoinColumn(nullable: false)]
     private ?Utilisateur $utilisateur = null;
 
+    #[ORM\ManyToOne(inversedBy: 'emprunts')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Livre $livre = null;
+
     public function getId(): ?int
     {
         return $this->id;
@@ -45,6 +49,18 @@ class Emprunt
     public function setUtilisateur(?Utilisateur $utilisateur): static
     {
         $this->utilisateur = $utilisateur;
+
+        return $this;
+    }
+
+    public function getLivre(): ?Livre
+    {
+        return $this->livre;
+    }
+
+    public function setLivre(?Livre $livre): static
+    {
+        $this->livre = $livre;
 
         return $this;
     }
